d73373f8e0c39dddbbb3ed2312f8acd2340061b0
wonderful <wonderful27x@126.com>
2020-07-12 10:44:50 +0800

    branch

--- app/src/main/java/com/example/gitbranchtest/TestClass.java
+++ app/src/main/java/com/example/gitbranchtest/TestClass.java
@@ -3,4 +3,20 @@ package com.example.gitbranchtest;
 public class TestClass {
     private String name;
     private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
